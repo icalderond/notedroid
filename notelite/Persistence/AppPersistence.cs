29f87872d755afd3167b1edfbf026b8f67df5a75
israelcalderon <icalderond@outlook.com>
2016-11-16 15:54:31 -0600

    Clase para persistencia de datos

---
+++ notelite/Persistence/AppPersistence.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+
+namespace notelite
+{
+    public class AppPersistence
+    {
+        Activity activity;
+        public ISharedPreferences preferencias;//Obtiene las preferencias
+        public ISharedPreferencesEditor editor_preferencias;//Habilita la opcion para editar las preferencias
+
+        public AppPersistence(Activity _activity)
+        {
+            this.activity = _activity;
+            preferencias = activity.GetSharedPreferences("NOTEDROID_PREF", FileCreationMode.Private);
+            editor_preferencias = preferencias.Edit();
+        }
+    }
+}
