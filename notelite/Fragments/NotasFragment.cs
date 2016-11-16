8178e878440d5d492dbedbdee3928a933fda183a
israelcalderon <icalderond@outlook.com>
2016-11-16 15:24:59 -0600

    Agregando layout al fragment de notas

--- notelite/Fragments/NotasFragment.cs
+++ notelite/Fragments/NotasFragment.cs
@@ -16,19 +16,14 @@ namespace notelite
 {
     public class NotasFragment : Fragment
     {
-        public override void OnCreate(Bundle savedInstanceState)
-        {
-            base.OnCreate(savedInstanceState);
-
-            // Create your fragment here
-        }
+        View view;
+        public override void OnCreate(Bundle savedInstanceState) => base.OnCreate(savedInstanceState);
 
         public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
         {
-            // Use this to return your custom view for this Fragment
-            // return inflater.Inflate(Resource.Layout.YourFragment, container, false);
+            view = inflater.Inflate(Resource.Layout.NotasLayout, container, false);
 
-            return base.OnCreateView(inflater, container, savedInstanceState);
+            return view;
         }
     }
 }
