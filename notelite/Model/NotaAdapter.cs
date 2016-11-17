d5f9235beb372131d21f7d8ae89e2862e331ec96
israelcalderon <icalderond@outlook.com>
2016-11-16 18:31:12 -0600

    Agregando el adaptador para objetos en el listview

---
+++ notelite/Model/NotaAdapter.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Android.App;
+using Android.Views;
+using Android.Widget;
+using Java.Lang;
+
+namespace notelite
+{
+    public class NotaAdapter : BaseAdapter
+    {
+        Activity activity;
+        List<Nota> ListaNotas;
+        public NotaAdapter(Activity _activity, IEnumerable<Nota> _listaNotas)
+        {
+            activity = _activity;
+            ListaNotas = _listaNotas.ToList();
+        }
+
+        public override int Count
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override Java.Lang.Object GetItem(int position)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override long GetItemId(int position)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override View GetView(int position, View convertView, ViewGroup parent)
+        {
+            View view = convertView;
+            if (view == null)
+                view = activity.LayoutInflater.Inflate(Resource.Layout.ItemNotaTemplate, null);
+
+            var tvTitulo = view.FindViewById<EditText>(Resource.Id.tvTitulo);
+            var tvContenido = view.FindViewById<EditText>(Resource.Id.tvTitulo);
+            var tvFecha = view.FindViewById<EditText>(Resource.Id.tvFecha);
+
+            var currentNote = ListaNotas[position];
+
+            tvTitulo.Text = currentNote.Titulo;
+            tvContenido.Text = currentNote.Contenido;
+            tvFecha.Text = currentNote.FechaEdicion.ToString("F");
+
+            return view;
+        }
+    }
+}
--- notelite/Resources/Resource.designer.cs
+++ notelite/Resources/Resource.designer.cs
@@ -60,35 +60,44 @@ namespace notelite
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f060003
-			public const int btnEntrar = 2131099651;
-			
 			// aapt resource value: 0x7f060006
-			public const int btnGuardar = 2131099654;
-			
-			// aapt resource value: 0x7f060008
-			public const int btnNuevaNota = 2131099656;
-			
-			// aapt resource value: 0x7f060002
-			public const int etClave = 2131099650;
-			
-			// aapt resource value: 0x7f060007
-			public const int etContenido = 2131099655;
-			
-			// aapt resource value: 0x7f060005
-			public const int etTitulo = 2131099653;
-			
-			// aapt resource value: 0x7f060001
-			public const int etUsuario = 2131099649;
-			
-			// aapt resource value: 0x7f060004
-			public const int fragment_container = 2131099652;
-			
-			// aapt resource value: 0x7f060000
-			public const int linearLayout1 = 2131099648;
+			public const int btnEntrar = 2131099654;
 			
 			// aapt resource value: 0x7f060009
-			public const int lvNotas = 2131099657;
+			public const int btnGuardar = 2131099657;
+			
+			// aapt resource value: 0x7f06000b
+			public const int btnNuevaNota = 2131099659;
+			
+			// aapt resource value: 0x7f060005
+			public const int etClave = 2131099653;
+			
+			// aapt resource value: 0x7f06000a
+			public const int etContenido = 2131099658;
+			
+			// aapt resource value: 0x7f060008
+			public const int etTitulo = 2131099656;
+			
+			// aapt resource value: 0x7f060004
+			public const int etUsuario = 2131099652;
+			
+			// aapt resource value: 0x7f060007
+			public const int fragment_container = 2131099655;
+			
+			// aapt resource value: 0x7f060003
+			public const int linearLayout1 = 2131099651;
+			
+			// aapt resource value: 0x7f06000c
+			public const int lvNotas = 2131099660;
+			
+			// aapt resource value: 0x7f060002
+			public const int tvContenido = 2131099650;
+			
+			// aapt resource value: 0x7f060001
+			public const int tvFecha = 2131099649;
+			
+			// aapt resource value: 0x7f060000
+			public const int tvTitulo = 2131099648;
 			
 			static Id()
 			{
@@ -104,16 +113,19 @@ namespace notelite
 		{
 			
 			// aapt resource value: 0x7f040000
-			public const int LoginLayout = 2130968576;
+			public const int ItemNotaTemplate = 2130968576;
 			
 			// aapt resource value: 0x7f040001
-			public const int Main = 2130968577;
+			public const int LoginLayout = 2130968577;
 			
 			// aapt resource value: 0x7f040002
-			public const int NotaLayout = 2130968578;
+			public const int Main = 2130968578;
 			
 			// aapt resource value: 0x7f040003
-			public const int NotasLayout = 2130968579;
+			public const int NotaLayout = 2130968579;
+			
+			// aapt resource value: 0x7f040004
+			public const int NotasLayout = 2130968580;
 			
 			static Layout()
 			{
